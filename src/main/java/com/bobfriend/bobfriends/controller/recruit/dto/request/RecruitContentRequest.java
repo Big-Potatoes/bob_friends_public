acb1bf8daa88397c4bc713b2eed85f125cd76169
한태현 <han@hantaehyeon-MacBookPro.local>
2023-03-25 02:00:28 +0900

    refactor: 파라미터 기본값 설정

--- src/main/java/com/bobfriend/bobfriends/controller/recruit/dto/request/RecruitContentRequest.java
+++ src/main/java/com/bobfriend/bobfriends/controller/recruit/dto/request/RecruitContentRequest.java
@@ -1,6 +1,7 @@
 package com.bobfriend.bobfriends.controller.recruit.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
@@ -9,7 +10,9 @@ import org.springframework.data.domain.Pageable;
 @Getter
 @Setter
 public class RecruitContentRequest {
+    @Schema(description = "페이지 번호 (1 이하는 0페이지로 자동 변환)", defaultValue="1")
     private int pageNumber = 1;
+    @Schema(description = "페이지 사이즈", defaultValue="5")
     private int pageSize = 5;
 
     public int getPageNumber() {
