0f87f6b0483bbdc2c640980d736da03ce5f9b855
한태현 <han@hantaehyeon-MacBookPro.local>
2023-03-25 00:17:38 +0900

    first commit

---
+++ src/main/java/com/bobfriend/bobfriends/BobFriendsServerApplication.java
@@ -0,0 +1,13 @@
+package com.bobfriend.bobfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobFriendsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobFriendsServerApplication.class, args);
+    }
+
+}
